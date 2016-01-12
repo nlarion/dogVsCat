bd3dd3ee113d82061cc15bce5d0b9d65e856ac25
Matthew Newell and Neil Larion <mnewell10@gmail.com>
2016-01-12 15:05:36 -0800

    add background buttons, and fix dog push to talk

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,9 @@
 $(document).ready(function() {
-  $("button").click(function() {
+  $("button#catButton, button#dogButton").click(function() {
     $("ul#dog").prepend("<li>Bark!</li>");
     $("ul#cat").prepend("<li>Meow!</li>");
   });
+
   $("h2#dogPic").click(function(){
     $("button#dogButton").after("<img src=\"images/dog.jpg\" alt=\"\" />");
     $(".col-sm-6").children("img").first().click(function(){
@@ -12,10 +13,27 @@ $(document).ready(function() {
 
   $("h2#catPic").click(function(){
     $("button#catButton").after("<img src=\"images/cat.jpg\" alt=\"\" />");
-    $(".col-sm-6").children("img").first().click(function(){
+    $("#attracDIV").children("img").first().click(function(){
       $(this).remove();
     });
   });
+
+  $(document).ready(function() {
+  $("button#green").click(function() {
+    $("body").removeClass();
+    $("body").addClass("green-background");
+  });
+
+  $("button#yellow").click(function() {
+    $("body").removeClass();
+    $("body").addClass("yellow-background");
+  });
+
+  $("button#red").click(function() {
+    $("body").removeClass();
+    $("body").addClass("red-background");
+  });
+});
 
 
 });
